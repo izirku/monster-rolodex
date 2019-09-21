87b0c2c76c38f5fad61b0c32d216219e3ed53eba
izirku <izirku@users.noreply.github.com>
2019-09-20 22:25:33 -0500

    Updates

---
+++ precache-manifest.a2da3bf0df1ef1c0d7e2a19a123abfbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c340e0b6c62d7e1408789a9bda10cc2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82e76a7390946098202e",
+    "url": "/static/css/main.8fabde5d.chunk.css"
+  },
+  {
+    "revision": "df5262d43648788e3c18",
+    "url": "/static/js/2.f3ca11ba.chunk.js"
+  },
+  {
+    "revision": "82e76a7390946098202e",
+    "url": "/static/js/main.f5e0a815.chunk.js"
+  },
+  {
+    "revision": "06a4e57344e54c124259",
+    "url": "/static/js/runtime-main.4201f264.js"
+  }
+]);
