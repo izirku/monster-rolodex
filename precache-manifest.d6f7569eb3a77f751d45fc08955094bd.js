fa16e18a33485fe21319d99850a807afd220b1d5
izirku <izirku@users.noreply.github.com>
2019-09-20 22:37:41 -0500

    Updates

---
+++ precache-manifest.d6f7569eb3a77f751d45fc08955094bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "371725f3ddcc1f68ea8e5261bbec0181",
+    "url": "/robot-rolodex/index.html"
+  },
+  {
+    "revision": "4cdc3ff92cc4481ef0ae",
+    "url": "/robot-rolodex/static/css/main.8fabde5d.chunk.css"
+  },
+  {
+    "revision": "df5262d43648788e3c18",
+    "url": "/robot-rolodex/static/js/2.f3ca11ba.chunk.js"
+  },
+  {
+    "revision": "4cdc3ff92cc4481ef0ae",
+    "url": "/robot-rolodex/static/js/main.a0c997ec.chunk.js"
+  },
+  {
+    "revision": "3b2acadd47b2938b3b84",
+    "url": "/robot-rolodex/static/js/runtime-main.997b563b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a2da3bf0df1ef1c0d7e2a19a123abfbb.js"
+  "/robot-rolodex/precache-manifest.d6f7569eb3a77f751d45fc08955094bd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robot-rolodex/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
